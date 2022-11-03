d53faf9525ac8e0151180165267425c736b5abf6
Supakorn Charoen <supakorncharoen@gmail.com>
2022-11-03 23:10:33 +0700

    [02-setup] - install ui dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Button } from 'antd';
+
 function App() {
-  return <div className="App">React App</div>;
+  return (
+    <div>
+      <div className="App">React App</div>
+      <Button type="primary">Clickme</Button>
+    </div>
+  );
 }
 
 export default App;
