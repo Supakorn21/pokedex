b92d9bccdb8b341d264ee43add1613573d004dac
Supakorn Charoen <supakorncharoen@gmail.com>
2022-11-08 16:18:46 +0700

    [02-setup] - restyling css and setup background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Button } from 'antd';
+import pokemonLogoSrc from './assets/images/pokedex.png';
+import { Image } from 'antd';
 
 function App() {
   return (
     <div>
       <div className="App">React App</div>
+      <Image width={200} src={pokemonLogoSrc} />
       <Button type="primary">Clickme</Button>
     </div>
   );
