8aebb4ea7eafbb913ac0c89545b573881e2b1499
Supakorn Charoen <supakorncharoen@gmail.com>
2022-11-08 17:03:46 +0700

    [02-setup] - set alias path and profile folder structure with atomic system

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { Button } from 'antd';
 import pokemonLogoSrc from './assets/images/pokedex.png';
 import { Image } from 'antd';
+import { Button } from '@atomic';
+import { log } from '@utils';
 
 function App() {
+  log('hello');
   return (
     <div>
       <div className="App">React App</div>
---
+++ src/components/atomic/atom/Button/Button.jsx
@@ -0,0 +1,7 @@
+import { Button as ButtonAntd } from 'antd';
+
+const Button = ({ children }) => {
+  return <ButtonAntd>{children}</ButtonAntd>;
+};
+
+export default Button;
