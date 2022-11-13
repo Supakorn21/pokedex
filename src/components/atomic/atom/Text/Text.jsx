e0ef09af484a288dc3b305449afc6a8280837930
Supakorn Charoen <supakorncharoen@gmail.com>
2022-11-13 14:54:17 +0700

    [04-pokemon-info-page] - pokemoninfo card

--- src/components/atomic/atom/Text/Text.jsx
+++ src/components/atomic/atom/Text/Text.jsx
@@ -1,11 +1,16 @@
+import styled from 'styled-components';
 import { Typography } from 'antd';
 const { Text: TextAntd } = Typography;
 
+const StyledText = styled(TextAntd)`
+  font-size: ${({ fontSize }) => fontSize || '1rem'};
+`;
+
 const Text = ({ children, style, ...props }) => {
   return (
-    <TextAntd style={style} {...props}>
+    <StyledText style={style} {...props}>
       {children}
-    </TextAntd>
+    </StyledText>
   );
 };
 
--- src/components/atomic/page/PokemonInfoPage/PokemonInfoPage.jsx
+++ src/components/atomic/page/PokemonInfoPage/PokemonInfoPage.jsx
@@ -1,10 +1,43 @@
+import styled from 'styled-components';
 import { useSearchParams } from 'react-router-dom';
+import { Row, Col } from 'antd';
+
+import { Card, PokemonInfo, PokemonData } from '@atomic';
+
+import { pokemonInfo, getCardColorsByPokemonTypes } from '@/utils';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+  align-items: center;
+`;
 
 const PokemonInfoPage = () => {
   let [searchParams] = useSearchParams();
 
-  const id = searchParams.get('id');
-  return <div>PokemonInfoPage : {id}</div>;
+  // const id = searchParams.get('id');
+  const bgColors = getCardColorsByPokemonTypes(pokemonInfo?.types);
+
+  return (
+    <Wrapper>
+      <Card
+        bgColors={bgColors}
+        width={'80%'}
+        borderRadius={'1rem'}
+        maxWidth={'80rem'}
+      >
+        <Row>
+          <Col xs={24} sm={12} md={8} xxl={8}>
+            <PokemonInfo pokemon={pokemonInfo} />
+          </Col>
+          <Col xs={24} sm={12} md={16} xxl={16}>
+            <PokemonData pokemon={pokemonInfo} />
+          </Col>
+        </Row>
+      </Card>
+    </Wrapper>
+  );
 };
 
 export default PokemonInfoPage;
